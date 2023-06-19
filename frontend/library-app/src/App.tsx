170b880433409d0c1901dbade0612cfaaf8b797b
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-06-19 22:32:31 +0600

    Explore Top Books Component

--- frontend/library-app/src/App.tsx
+++ frontend/library-app/src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
+  );
 }
 
 export default App;
