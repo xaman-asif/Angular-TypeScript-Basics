634142df8b0f235bea1ece6ded00e5e83827ae48
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-09-20 16:18:20 +0600

    Dynamic Form Controls

--- angular-forms/reactive-forms/src/app/app.component.ts
+++ angular-forms/reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ForbiddenNameValidator} from "./shared/user-name.validators";
 import {PasswordValidator} from "./shared/password.validator";
 
@@ -8,35 +8,48 @@ import {PasswordValidator} from "./shared/password.validator";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  registrationForm = this.formBuilder.group({
-    userName: ['', [Validators.required, Validators.minLength(4), ForbiddenNameValidator([/password/, /admin/])]],
-    password: [''],
-    confirmPassword: [''],
-
-    address: this.formBuilder.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  }, {validator: PasswordValidator})
+export class AppComponent implements OnInit {
+  registrationForm: FormGroup = new FormGroup({})
 
   constructor(private formBuilder: FormBuilder) {
   }
 
+  get userName() {
+    return this.registrationForm.get('userName')
+  }
 
-  // registrationForm = new FormGroup({
-  //   userName: new FormControl(''),
-  //   password: new FormControl(''),
-  //   confirmPassword: new FormControl(''),
-  //
-  //   address: new FormGroup({
-  //     city: new FormControl(''),
-  //     state: new FormControl(''),
-  //     postalCode: new FormControl(''),
-  //   })
-  // });
+  get email() {
+    return this.registrationForm.get('email')
+  }
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.minLength(4), ForbiddenNameValidator([/password/, /admin/])]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+
+      address: this.formBuilder.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    }, {validator: PasswordValidator})
+
+    this.registrationForm.get('subscribe')?.valueChanges.subscribe(
+      checkedValue => {
+        const email = this.registrationForm.get('email');
+        if (checkedValue) {
+          email?.setValidators(Validators.required)
+        } else {
+          email?.clearValidators()
+        }
+
+        email?.updateValueAndValidity();
+      }
+    )
+  }
 
   loadApiData() {
     this.registrationForm.setValue({
@@ -51,4 +64,16 @@ export class AppComponent {
       }
     })
   }
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   })
+  // });
 }
