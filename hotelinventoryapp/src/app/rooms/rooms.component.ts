c725f9a5fcd3d2521200d937f7d0e42fc0923314
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-10-15 00:08:06 +0600

    Built-in Directives

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -11,6 +12,29 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
 
   hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 15,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Duluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      roomPrice: 500,
+      checkInTime: new Date(),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Gorib Room',
+      amenities: 'Fan, No Wi-Fi, Radio, Toilet, Food Order',
+      roomPrice: 100,
+      checkInTime: new Date(),
+    },
+  ];
 
   constructor() {}
 
---
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -0,0 +1,13 @@
+export interface Room {
+  totalRooms: number;
+  availableRooms: number;
+  bookedRooms: number;
+}
+
+export interface RoomList {
+  roomNumber: number;
+  roomType: string;
+  roomPrice: number;
+  amenities: string;
+  checkInTime: Date;
+}
