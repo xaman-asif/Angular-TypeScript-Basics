8026d56a226fc90ff3d4dd2975401e64156b44ab
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-10-17 23:18:52 +0600

    ngOnInt and Component Communication using Input and Output

---
+++ hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RoomList } from '../rooms';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss'],
+})
+export class RoomsListComponent implements OnInit {
+  @Input() rooms: RoomList[] = [];
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
+}
