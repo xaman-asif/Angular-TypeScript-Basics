abf20ee89e0fa25b13ec3e6ad62d8bfda37b0671
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2025-01-09 18:36:50 +0600

    Routing Basics/CanActivate

---
+++ hotelinventoryapp/src/app/features/login/login.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  isLoggedIn = false;
+
+  constructor() {
+  }
+
+  login(email: string, password: string) {
+    if (email === "admin@gmail.com" && password === "Admin") {
+      this.isLoggedIn = true;
+    }
+
+    return this.isLoggedIn;
+  }
+}
