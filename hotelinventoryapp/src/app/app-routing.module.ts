5c18d86937aceebc1c5076b7fadcee866425ff52
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2025-01-09 18:36:40 +0600

    Routing Basics/CanActivate

--- hotelinventoryapp/src/app/app-routing.module.ts
+++ hotelinventoryapp/src/app/app-routing.module.ts
@@ -3,22 +3,26 @@ import {NgModule} from "@angular/core";
 import {EmployeeComponent} from "./features/employee/employee.component";
 import {NotFoundComponent} from "./features/not-found/not-found.component";
 import {LoginComponent} from "./features/login/login.component";
+import {LoginGuard} from "./core/guards/login.guard";
 
 const routes: Routes = [{
   path: 'employee',
-  component: EmployeeComponent
+  component: EmployeeComponent,
+  canActivate: [LoginGuard]
 }, {
   path: 'login',
   component: LoginComponent
 }, {
   path: 'rooms',
-  loadChildren: () => import ('./features/rooms/rooms.module').then(m => m.RoomsModule)
+  loadChildren: () => import ('./features/rooms/rooms.module').then(m => m.RoomsModule),
+  canActivate: [LoginGuard]
 }, {
   path: 'booking',
   loadChildren: () => import('./features/booking/booking.module').then(m => m.BookingModule)
 }, {
   path: '**',
-  component: NotFoundComponent
+  component: NotFoundComponent,
+  canActivate: [LoginGuard]
 }];
 
 @NgModule(
--- hotelinventoryapp/src/app/features/login/login.component.ts
+++ hotelinventoryapp/src/app/features/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {LoginService} from "./login.service";
 
 @Component({
   selector: 'hinv-login',
@@ -10,16 +11,17 @@ export class LoginComponent implements OnInit {
   email !: string;
   password !: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private loginService: LoginService) {
   }
 
   ngOnInit(): void {
   }
 
   login() {
-    if (this.email === "admin@gmail.com" && this.password === "Admin") {
-      // alert("Login Successfully")
-      this.router.navigate(['/rooms', 'add']);
+
+    if (this.loginService.login(this.email, this.password)) {
+      this.router.navigate(['/rooms'])
     }
   }
 }
