030eebf851c9f927596073f0c3159aec080a2683
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2025-01-09 18:11:20 +0600

    Routing Basics/Listening to Router Events

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {localStorageToken} from "./core/models/localstorage.token";
 import {sessionStorageToken} from "./core/models/sessionstorage.token";
 import {InitService} from "./core/services/init.service";
-import {RoomsService} from "./features/rooms/services/rooms.service";
+import {NavigationEnd, NavigationStart, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'hinv-root',
@@ -12,11 +13,31 @@ import {RoomsService} from "./features/rooms/services/rooms.service";
 export class AppComponent implements OnInit {
 
   constructor(@Inject(localStorageToken) private localStorage: Storage,
-              @Inject(sessionStorageToken) private sessionStorage: Storage, private initService: InitService, private roomService: RoomsService) {
+              @Inject(sessionStorageToken) private sessionStorage: Storage,
+              private initService: InitService,
+              private router: Router) {
     console.log(initService.config);
   }
 
   ngOnInit(): void {
+    // this.router.events.subscribe((event) => {
+    //   console.log(event);
+    // })
+
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationStart)
+      ).subscribe((event) => {
+      console.log("Navigation Started");
+    })
+
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd)
+      ).subscribe((event) => {
+      console.log("Navigation Completed");
+    })
+
     this.localStorage.setItem('name', 'Syng frm localstorage');
     this.sessionStorage.setItem('name', 'Syng frm sessionStorage');
   }
