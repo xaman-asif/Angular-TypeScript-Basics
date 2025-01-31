8c0383c1b8621ec74dfe77e6cbef0ae6a3c80821
Asif Jaman <asifuzzaman.opi@gmail.com>
2025-01-31 13:00:41 +0600

    Error Handling/Error Handling

--- hotelinventoryapp/src/app/app.module.ts
+++ hotelinventoryapp/src/app/app.module.ts
@@ -1,30 +1,33 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {EmployeeComponent} from './features/employee/employee.component';
-import {APP_CONFIG, APP_SERVICE_CONFIG} from "./core/services/appconfig.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {RequestInterceptor} from "./core/interceptors/request.interceptor";
-import {InitService} from "./core/services/init.service";
-import {AppRoutingModule} from "./app-routing.module";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppNavComponent} from './shared/components/app-nav/app-nav.component';
-import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {NotFoundComponent} from './features/not-found/not-found.component';
-import {FormsModule} from "@angular/forms";
-import {LoginComponent} from './features/login/login.component';
-import {HoverDirective} from './shared/directives/hover/hover.directive';
-import {EmailValidatorDirective} from './shared/directives/email-validator/email-validator.directive';
-import {HeaderComponent} from "./features/header/header.component";
-import {HeaderModule} from "./features/header/header.module";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RequestInterceptor } from './core/interceptors/request.interceptor';
+import {
+  APP_CONFIG,
+  APP_SERVICE_CONFIG,
+} from './core/services/appconfig.service';
+import { GlobalErrorHandler } from './core/services/errorhandler.service';
+import { InitService } from './core/services/init.service';
+import { EmployeeComponent } from './features/employee/employee.component';
+import { HeaderComponent } from './features/header/header.component';
+import { HeaderModule } from './features/header/header.module';
+import { LoginComponent } from './features/login/login.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { AppNavComponent } from './shared/components/app-nav/app-nav.component';
+import { EmailValidatorDirective } from './shared/directives/email-validator/email-validator.directive';
+import { HoverDirective } from './shared/directives/hover/hover.directive';
 
 @NgModule({
   declarations: [
@@ -34,30 +37,48 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
     NotFoundComponent,
     LoginComponent,
     HoverDirective,
-    EmailValidatorDirective
+    EmailValidatorDirective,
   ],
-  imports: [HeaderModule,
-    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, FormsModule, MatSnackBarModule],
-  providers: [{
-    provide: APP_SERVICE_CONFIG,
-    useValue: APP_CONFIG
-  }, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: RequestInterceptor,
-    multi: true
-  }, {
-    provide: APP_INITIALIZER,
-    useFactory: initFactory,
-    deps: [InitService],
-    multi: true
-  }],
-  exports: [
-    HeaderComponent
+  imports: [
+    HeaderModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FormsModule,
+    MatSnackBarModule,
   ],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_SERVICE_CONFIG,
+      useValue: APP_CONFIG,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initFactory,
+      deps: [InitService],
+      multi: true,
+    },
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+    },
+  ],
+  exports: [HeaderComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
 
 function initFactory(initService: InitService) {
   return () => initService.init();
--- hotelinventoryapp/src/app/features/comments/service/comment.service.ts
+++ hotelinventoryapp/src/app/features/comments/service/comment.service.ts
@@ -1,6 +1,6 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {Comment} from '../interface/comment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Comment } from '../interface/comment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class CommentService {
 
   getComments() {
     return this.http.get<Comment[]>(
-      'https://jsonplaceholder.typicode.com/comments'
+      'https://jsonplaceholder.typicode.com/commentsyxd'
     );
   }
 }
